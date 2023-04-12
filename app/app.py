1b2b4b880eec48f953f3cc08a41546ca219eb851
Charles Stark <charlesstark918@gmail.com>
2023-04-12 20:59:00 +0800

    update

--- app/app.py
+++ app/app.py
@@ -9,45 +9,43 @@ from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
 
-class InputDialog(QWidget):
+class InputWindow(QWidget):
 
     def __init__(self, parent=None):
-        super(InputDialog, self).__init__(parent)
+        super(InputWindow, self).__init__(parent)
+
         layout = QFormLayout()
 
-        self.btn1 = QPushButton("获得列表里的选项")
-        self.btn1.clicked.connect(self.getItem)
-        self.le1 = QLineEdit()
-        layout.addRow(self.btn1, self.le1)
+        self.label1 = QLabel()
+        self.label1.setText('校园网')
+        layout.addRow(self.label1)
 
-        self.btn2 = QPushButton("获得字符串")
-        self.btn2.clicked.connect(self.getIext)
-        self.le2 = QLineEdit()
-        layout.addRow(self.btn2, self.le2)
+        self.label2 = QLabel()
+        self.label2.setText('用户名')
+        self.lineEdit2 = QLineEdit()
+        layout.addRow(self.label2, self.lineEdit2)
 
-        self.btn3 = QPushButton("获得整数")
-        self.btn3.clicked.connect(self.getInt)
-        self.le3 = QLineEdit()
-        layout.addRow(self.btn3, self.le3)
+        self.label3 = QLabel()
+        self.label3.setText('密码')
+        self.lineEdit3 = QLineEdit()
+        layout.addRow(self.label3, self.lineEdit3)
+
+        self.label4 = QLabel()
+        self.label4.setText('VPN')
+        layout.addRow(self.label4)
+
+        self.label5 = QLabel()
+        self.label5.setText('用户名')
+        self.lineEdit5 = QLineEdit()
+        layout.addRow(self.label5, self.lineEdit5)
+
+        self.label6 = QLabel()
+        self.label6.setText('密码')
+        self.lineEdit6 = QLineEdit()
+        layout.addRow(self.label6, self.lineEdit6)
+
         self.setLayout(layout)
-        self.setWindowTitle("Input Dialog 示例")
-
-    def getItem(self):
-        items = ("C", "C++", "Java", "Python")
-        item, ok = QInputDialog.getItem(self, "select input dialog",
-                                        "语言列表", items, 0, False)
-        if ok and item:
-            self.le1.setText(item)
-
-    def getIext(self):
-        text, ok = QInputDialog.getText(self, 'Text Input Dialog', '输入姓名:')
-        if ok:
-            self.le2.setText(str(text))
-
-    def getInt(self):
-        num, ok = QInputDialog.getInt(self, "integer input dualog", "输入数字")
-        if ok:
-            self.le3.setText(str(num))
+        self.setWindowTitle('添加账户')
 
 
 class Tray(QSystemTrayIcon):
@@ -92,9 +90,10 @@ class Tray(QSystemTrayIcon):
 
         self.setContextMenu(self.menu)
 
+        self.window = InputWindow()
+
     def input_info(self):
-        window = InputDialog()
-        window.show()
+        self.window.show()
 
     def connect(self):
         pass
